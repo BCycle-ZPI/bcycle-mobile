9d678fa74f9614e3aa6272cc2508792f5068531d
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2020-03-05 10:14:08 +0100

    Initial commit

---
+++ app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package pl.pwr.zpi.bcycle.mobile.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
