a02729d39a54f5e045644b7ccea6240d18033b47
Magda Wójcik <48806197+meg25ellele@users.noreply.github.com>
2020-04-09 21:29:57 +0200

    avatars loaded

--- app/src/main/java/pl/pwr/zpi/bcycle/mobile/RegisterActivity.kt
+++ app/src/main/java/pl/pwr/zpi/bcycle/mobile/RegisterActivity.kt
@@ -1,22 +1,29 @@
 package pl.pwr.zpi.bcycle.mobile
 
+import android.Manifest
+import android.R.attr
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import com.squareup.picasso.Picasso
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_register.*
 import pl.pwr.zpi.bcycle.mobile.utils.content
 import pl.pwr.zpi.bcycle.mobile.utils.showToast
 
 
 class RegisterActivity : AppCompatActivity() {
-
 
 
     private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
@@ -30,6 +37,9 @@ class RegisterActivity : AppCompatActivity() {
         galleryBt.setOnClickListener {
             loadPhotoFromGallery()
         }
+         cameraBt.setOnClickListener{
+             loadPhotoFromCamera()
+         }
 
         registerBt.isEnabled = false
         registerBt.setOnClickListener {
@@ -39,12 +49,66 @@ class RegisterActivity : AppCompatActivity() {
         privacyCB.setOnCheckedChangeListener { _, isChecked -> registerBt.isEnabled = isChecked }
     }
 
+    private fun loadPhotoFromCamera() {
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                val permission= arrayOf(Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permission,PERMISSION_CODE_CAMERA)
+            } else {
+                openCamera()
+            }
+        } else  {
+            openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        val values  = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New avatar")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the camera")
+        mImageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri)
+        startActivityForResult(cameraIntent,MAKE_IMAGE_REQUEST )
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when(requestCode) {
+            PERMISSION_CODE_CAMERA -> if(grantResults.size>0  && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                                    openCamera()
+            PERMISSION_CODE_GALLERY -> if(grantResults.size>0  && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                                    openGallery()
+            else -> showToast("Permission denied...")
+
+        }
+    }
 
     private fun loadPhotoFromGallery() {
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                val permission= arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                requestPermissions(permission, PERMISSION_CODE_GALLERY)
+            } else {
+                openGallery()
+            }
+        } else  {
+            openGallery()
+        }
+
+    }
+
+    private fun openGallery() {
         intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -53,10 +117,35 @@ class RegisterActivity : AppCompatActivity() {
                 && data !=null && data.data != null){
 
             mImageUri = data.data
-            Picasso.get().load(mImageUri).into(avatarImV)
+
+                CropImage.activity(mImageUri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+
+        }
+
+        if(requestCode == MAKE_IMAGE_REQUEST && mImageUri !=null) {
+
+            CropImage.activity(mImageUri)
+                .setAspectRatio(1,1)
+                .start(this);
+
+        }
+
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+
+            val result : CropImage.ActivityResult = CropImage.getActivityResult(data)
+
+            if(resultCode == Activity.RESULT_OK) {
+
+                mImageUri = result.uri
+
+                Picasso.get().load(mImageUri).into(avatarImV)
+
+            }
+
         }
     }
-
 
     // TODO: Implement actual form validation
     private fun isFormValid(): Boolean =
@@ -100,9 +189,13 @@ class RegisterActivity : AppCompatActivity() {
         return false
     }
 
+
     companion object {
+        private val PERMISSION_CODE_CAMERA: Int = 1000;
+        private val PERMISSION_CODE_GALLERY: Int = 1001;
         private var mImageUri: Uri? = null
-        private final val PICK_IMAGE_REQUEST = 1
+        private  val PICK_IMAGE_REQUEST = 1
+        private  val MAKE_IMAGE_REQUEST = 2
         private val defaultAvatarUri =
             Uri.parse("${FirebaseStorage.getInstance().reference.root}/default_avatar.png")
     }
