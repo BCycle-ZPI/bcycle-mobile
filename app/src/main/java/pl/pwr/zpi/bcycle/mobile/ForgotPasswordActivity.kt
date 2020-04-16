c4712396080e273b0025ac54908c9134e822ee7a
Magda Wójcik <48806197+meg25ellele@users.noreply.github.com>
2020-04-16 16:56:18 +0200

    done

--- app/src/main/java/pl/pwr/zpi/bcycle/mobile/ForgotPasswordActivity.kt
+++ app/src/main/java/pl/pwr/zpi/bcycle/mobile/ForgotPasswordActivity.kt
@@ -1,14 +1,62 @@
 package pl.pwr.zpi.bcycle.mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_forgot_password.*
+import pl.pwr.zpi.bcycle.mobile.utils.content
+import pl.pwr.zpi.bcycle.mobile.utils.showToast
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private var allControls: List<View> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
         supportActionBar?.hide()
+        allControls = listOf(emailET,sendEmailBt)
+
+        sendEmailBt.setOnClickListener() {
+            if(isNotEmailEmpty()) sendEmail()
+        }
+    }
+
+    private fun isNotEmailEmpty() : Boolean {
+        if(emailET.content().isEmpty()) {
+            emailET.error = getString(R.string.empty_edit_text)
+        }
+
+        return  emailET.content().isNotEmpty()
+    }
+
+    private fun sendEmail() {
+
+        showSpinnerAndDisableControls()
+        auth.sendPasswordResetEmail(emailET.content())
+            .addOnSuccessListener {
+                showToast(getString(R.string.email_sent_message))
+                startActivity(Intent(this,LoginActivity::class.java))
+                finish()
+
+            }.addOnFailureListener(){
+                showToast("${getString(R.string.failed_to_sign_in)} + ${it.localizedMessage}")
+                hideSpinnerAndEnableControls()
+            }
+
+    }
+
+    private fun showSpinnerAndDisableControls() {
+        progressBar.visibility = View.VISIBLE
+        allControls.forEach { v -> v.isEnabled = false }
+    }
+
+    private fun hideSpinnerAndEnableControls() {
+        progressBar.visibility = View.GONE
+        allControls.forEach { v -> v.isEnabled = true }
     }
 }
