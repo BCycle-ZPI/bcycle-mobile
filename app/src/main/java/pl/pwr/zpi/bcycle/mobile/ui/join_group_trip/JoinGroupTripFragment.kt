94e841b4c7f3b426c5d930c5d114e0867d74de41
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2020-04-23 00:59:18 +0200

    Rename fragments to match navigation drawer

--- app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/join_group_trip/JoinGroupTripFragment.kt
+++ app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/join_group_trip/JoinGroupTripFragment.kt
@@ -1,4 +1,4 @@
-package pl.pwr.zpi.bcycle.mobile.ui.gallery
+package pl.pwr.zpi.bcycle.mobile.ui.join_group_trip
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,9 +10,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import pl.pwr.zpi.bcycle.mobile.R
 
-class GalleryFragment : Fragment() {
+class JoinGroupTripFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+    private lateinit var joinGroupTripViewModel: JoinGroupTripViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,10 +20,10 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        galleryViewModel = ViewModelProvider(this).get(GalleryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+        joinGroupTripViewModel = ViewModelProvider(this).get(JoinGroupTripViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_join_group_trip, container, false)
+        val textView: TextView = root.findViewById(R.id.text_join_group_trip)
+        joinGroupTripViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/join_group_trip/JoinGroupTripViewModel.kt
+++ app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/join_group_trip/JoinGroupTripViewModel.kt
@@ -1,13 +1,13 @@
-package pl.pwr.zpi.bcycle.mobile.ui.tools
+package pl.pwr.zpi.bcycle.mobile.ui.join_group_trip
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ToolsViewModel : ViewModel() {
+class JoinGroupTripViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is tools Fragment"
+        value = "This is join_group_trip Fragment"
     }
     val text: LiveData<String> = _text
 }
