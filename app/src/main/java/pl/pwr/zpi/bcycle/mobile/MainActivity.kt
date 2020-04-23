e3df932d41014fb591be76f54776fccf2b5d354f
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2020-04-23 10:56:59 +0200

    Make navigation work in RecordTripActivity

--- app/src/main/java/pl/pwr/zpi/bcycle/mobile/MainActivity.kt
+++ app/src/main/java/pl/pwr/zpi/bcycle/mobile/MainActivity.kt
@@ -3,32 +3,20 @@ package pl.pwr.zpi.bcycle.mobile
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.nav_header_main.view.*
 import pl.pwr.zpi.bcycle.mobile.api.ApiTokenManager
 import pl.pwr.zpi.bcycle.mobile.utils.showToast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BCycleNavigationDrawerActivity() {
 
     private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
     private val storage: FirebaseStorage by lazy { FirebaseStorage.getInstance() }
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,43 +29,19 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_join_group_trip, R.id.nav_create_group_trip,
-                R.id.nav_my_account, R.id.nav_settings
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        navView.setNavigationItemSelectedListener {
-            if (it.itemId == R.id.nav_logout) {
-                auth.signOut()
-                finish()
-            } else {
-                navController.navigate(it.itemId)
-            }
-            drawerLayout.closeDrawer(GravityCompat.START)
-            true
-        }
+        configureMainNavigationDrawer(auth)
 
         auth.currentUser?.run {
             ApiTokenManager.updateToken(this)
                 .addOnFailureListener {
                     showToast(getString(R.string.token_update_failed))
                 }
-            val header = navView.getHeaderView(0)
-            header.currentUserName.text = displayName
-            header.currentUserEmail.text = email
-            storage.getReferenceFromUrl(this.photoUrl.toString()).downloadUrl
-                .addOnSuccessListener{ Picasso.get().load(it).into(header.currentUserImage) }
-
+            updateNavigationDrawerHeader(this, storage)
         } ?: openLoginScreen()
+
+        if (intent.extras != null && intent.extras!!.containsKey(INTENT_EXTRA_MAIN_NAV_ID)) {
+            navController.navigate(intent.extras!!.getInt(INTENT_EXTRA_MAIN_NAV_ID))
+        }
     }
 
     private fun openLoginScreen() {
@@ -89,11 +53,6 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onResume() {
