ec46ba02991ad61050674685d582604f37aec838
Magda Wójcik <48806197+meg25ellele@users.noreply.github.com>
2020-05-06 14:39:56 +0200

    current user photo, name,email loaded

--- app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/my_account/MyAccountFragment.kt
+++ app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/my_account/MyAccountFragment.kt
@@ -8,11 +8,18 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_my_account.*
 import pl.pwr.zpi.bcycle.mobile.R
 
 class MyAccountFragment : Fragment() {
 
     private lateinit var myAccountViewModel: MyAccountViewModel
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val storage: FirebaseStorage by lazy { FirebaseStorage.getInstance() }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,4 +30,15 @@ class MyAccountFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_my_account, container, false)
         return root
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        nameTV.text = auth.currentUser?.displayName;
+        emailTV.text = auth.currentUser?.email;
+        storage.getReferenceFromUrl(auth.currentUser?.photoUrl.toString()).downloadUrl
+            .addOnSuccessListener { Picasso.get().load(it).into(avatarIV) }
+    }
+
+
 }
