8f9caa64232e0f7c48bbbfb37da77cbe1ff8b9a8
Magda Wójcik <48806197+meg25ellele@users.noreply.github.com>
2020-05-12 14:07:38 +0200

    stats fixed

--- app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/my_account/MyAccountFragment.kt
+++ app/src/main/java/pl/pwr/zpi/bcycle/mobile/ui/my_account/MyAccountFragment.kt
@@ -1,18 +1,20 @@
 package pl.pwr.zpi.bcycle.mobile.ui.my_account
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_my_account.*
 import pl.pwr.zpi.bcycle.mobile.R
+import pl.pwr.zpi.bcycle.mobile.api.ApiClient
+import pl.pwr.zpi.bcycle.mobile.utils.background
 
 class MyAccountFragment : Fragment() {
 
@@ -31,13 +33,35 @@ class MyAccountFragment : Fragment() {
         return root
     }
 
+    @SuppressLint("CheckResult")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        progressBar.visibility = View.VISIBLE
 
         nameTV.text = auth.currentUser?.displayName;
         emailTV.text = auth.currentUser?.email;
         storage.getReferenceFromUrl(auth.currentUser?.photoUrl.toString()).downloadUrl
             .addOnSuccessListener { Picasso.get().load(it).into(avatarIV) }
+
+
+        ApiClient.statsApi.getUserStats().background().subscribe({
+            stats ->
+            numberOfAllTripsCountTV.text = stats.result.tripCount.toString()
+            numberOfGroupTripsCountTV.text = stats.result.groupTripTotalCount.toString()
+            numberOfCreatedGroupTripsCountTV.text  = stats.result.groupTripHostingCount.toString()
+            totalDistanceKmCountTV.text = stats.result.totalKilometers.toString()
+            totalTimeCountTV.text = stats.result.totalTimeMinutes.toString()
+
+            progressBar.visibility = View.GONE
+        },
+            {error->
+                error.message
+                Toast.makeText(activity,getString(R.string.changed_data_error), Toast.LENGTH_SHORT).show()
+                progressBar.visibility=View.GONE;
+
+        }
+        )
     }
 
 
